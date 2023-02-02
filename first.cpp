e4deec77d4227c82e6017f52ebabe0e7d4789451
CoffeeNstuff <coffeenstuff0@gmail.com>
2023-02-01 21:14:18 -0500

    first commit

---
+++ first.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+// template <typename  u>
+enum compareAge {older, younger};
+void checkAge(int age);
+
+int main() {
+std::string age;
+std::cout << 'are you  older or younger than 18';
+std::cin >>  age; 
+
+return 0;
+}
+
+void checkAge(std::string age) {
+std::cout << "hello your " << age << " years old" << \n 
+}
+
